ee8b9607fa5a9eca38329d11f5f1097cabdde3dd
NH8 <756144105@qq.com>
2026-03-05 23:36:14 +0800

    style: 修改在线播放器样式

--- components/BiliPlayerModal.component.jsx
+++ components/BiliPlayerModal.component.jsx
@@ -6,36 +6,64 @@ import { eff_set } from "../config/controllers";
 
 export default function BiliPlayerModal
   ({ props: [ Title, Visible, List, Selected, EffThis, ] }) {
-  
-  const bvid_url = bvid => '//player.bilibili.com/player.html?bvid=' + bvid;
-  
+
+  const bvid_url = (bvid) => `//player.bilibili.com/player.html?bvid=${bvid}`;
+  const bvid_list = (Array.isArray(List) ? List : [])
+    .map((item) => (typeof item === "string" ? item.trim() : ""))
+    .filter(Boolean);
+  const selected_bvid = Selected || bvid_list[0] || "";
+
   return (
-    <Modal show = { Visible } onHide = { EffThis.hide_bili_player } fullscreen = "xl-down" size = "xl" centered>
-      <Modal.Header closeButton>
-        <Modal.Title>
+    <Modal
+      show={Visible}
+      onHide={EffThis.hide_bili_player}
+      fullscreen="xl-down"
+      size="xl"
+      centered
+    >
+      <Modal.Header closeButton className={styles.biliModalHeader}>
+        <Modal.Title className={styles.biliModalTitle}>
           { Title }
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <div className = { styles.biliPlayerDiv }>
-          <p className = { styles.bvid_bar }>{ List.map((bvid) => (
-            <span
-              className = { ( Selected === bvid
-                            ? styles.bvid_bar__item + ' ' + styles.bvid_bar__item__inactive
-                            : styles.bvid_bar__item
-                          ) }
-
-              onClick = { e => eff_set(EffThis, 'bvid_selected', e.currentTarget.textContent) }
-              
-              key = { bvid }
-            >
-              { bvid }
-            </span>)) }
-          </p>
-          <iframe src = { bvid_url(Selected) } width = "100%" height = "100%" scrolling = "no" border = "0" frameBorder = "no" allowFullScreen = { true }>
-          </iframe>
+      <Modal.Body className={styles.biliModalBody}>
+        <div className={styles.biliPlayerDiv}>
+          <div className={styles.bvid_bar}>
+            {bvid_list.map((bvid, idx) => (
+              <button
+                type="button"
+                className={
+                  selected_bvid === bvid
+                    ? `${styles.bvid_bar__item} ${styles.bvid_bar__item__inactive}`
+                    : styles.bvid_bar__item
+                }
+                onClick={() => eff_set(EffThis, "bvid_selected", bvid)}
+                key={`${bvid}-${idx}`}
+              >
+                { bvid }
+              </button>
+            ))}
+          </div>
+          <div className={styles.biliPlayerFrameWrap}>
+            {selected_bvid ? (
+              <iframe
+                src={bvid_url(selected_bvid)}
+                width="100%"
+                height="100%"
+                scrolling="no"
+                border="0"
+                frameBorder="0"
+                allowFullScreen={true}
+                title={`Bilibili Player - ${selected_bvid}`}
+              />
+            ) : (
+              <div className={styles.biliPlayerEmpty}>
+                暂无可播放视频
+              </div>
+            )}
+          </div>
         </div>
       </Modal.Body>
     </Modal>
-  )
+  );
 }
